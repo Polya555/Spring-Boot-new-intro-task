eb0d9e93e139557f1ce545689a491064f6e5acf8
пк <klimashevscapo@gmail.com>
2026-09-18 21:31:01 +0300

    changed BookController

--- src/main/java/mate/academy/controller/BookController.java
+++ src/main/java/mate/academy/controller/BookController.java
@@ -6,10 +6,10 @@ import mate.academy.dto.CreateBookRequestDto;
 import mate.academy.entity.Book;
 import mate.academy.mapper.BookMapper;
 import mate.academy.service.BookService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
