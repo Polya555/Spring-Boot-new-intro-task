901670858c134116ba194f7d0bd6efd668ca48f8
пк <klimashevscapo@gmail.com>
2026-09-24 20:46:44 +0300

    changed CreateBookRequestDto

--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,7 +1,7 @@
 package mate.academy.dto;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
